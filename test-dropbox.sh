1e81ca9c363142c5a4e836baf8684fd05d0e7634
Michael <30936296+mtan93@users.noreply.github.com>
2021-09-24 11:53:36 +0100

    Delete test-dropbox.sh

--- test-dropbox.sh
+++
@@ -1,7 +0,0 @@
-curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15" -L https://www.dropbox.com/download?plat=mac > /var/tmp/DropBoxInstaller.dmg
-hdiutil attach /var/tmp/DropBoxInstaller.dmg
-cd /Volumes/Dropbox Installer/Dropbox.app/Contents/MacOS/
-"./Dropbox Installer"
-hdiutil detach /Volumes/Dropbox Installer/
-rm -rf /var/tmp/DropBoxInstaller.dmg
-exit 0
