b0177af26f498b1d6050fe4badc84ca5c1fa04c7
Michael <30936296+mtan93@users.noreply.github.com>
2021-09-24 11:49:47 +0100

    Update test-dropbox.sh

--- test-dropbox.sh
+++ test-dropbox.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15" -L https://dl-web.dropbox.com/installer?build_no=131.4.3968&plat=mac&tag=DBPREAUTH%3A%3Achrome%3A%3AeJyrVkosLcmIL8nPTs1TslJQKi4oSDUwKglP9nALC88z9PEtiExLDshLzK0yi_APqQjTMzQzNjKxMDAxsVTSASpPLS7OzM-Lz0wBaja0tDCzMDIyNDQzMQYqs7Q0NjewNDYzNTE2MjAxNTMwNTQCIstaAEDVIKE~%40META&tag_token=AVJC7OhbTB-egpL1UZKEi2J8d -o /var/tmp/DropBoxInstaller.dmg
+curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15" -L https://www.dropbox.com/download?plat=mac > /var/tmp/DropBoxInstaller.dmg
 hdiutil attach /var/tmp/DropBoxInstaller.dmg
 cd /Volumes/Dropbox Installer/Dropbox.app/Contents/MacOS/
 ./Dropbox Installer
