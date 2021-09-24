520947ef50fbffbfc054e6a4981f915ea9096c5f
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-09-24 15:13:05 +0100

    Add files via upload

---
+++ Rosetta2-JAMF.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+arch=$(/usr/bin/arch)
+if [ "$arch" == "arm64" ]; then
+    echo "Apple Silicon - Installing Rosetta"
+    echo "Apple Silicon - Installing Rosetta" >> ${deplog}
+    sleep 1
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+    echo "Rosetta 2 Now Installed" >> ${deplog}
+    echo "Rosetta 2 Now Installed"
+    sleep 5
+elif [ "$arch" == "i386" ]; then
+    echo "Intel - Skipping Rosetta" >> ${deplog}
+    echo "Intel - Skipping Rosetta"
+    sleep 1
+else
+    echo "Unknown Architecture" >> ${deplog}
+    echo "Unknown Architecture"
+    sleep 1
+fi
+echo "Running recon so Architecture gets updated and arch based smart groups work" >> ${deplog}
+echo "Running recon so Architecture gets updated and arch based smart groups work"
+jamf recon
+exit 0
