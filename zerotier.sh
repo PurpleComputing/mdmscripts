94de22c9aca662924427e919de93c17886ff0d04
Michael <30936296+mtan93@users.noreply.github.com>
2021-09-28 11:36:47 +0100

    Update zerotier.sh

--- zerotier.sh
+++ zerotier.sh
@@ -1,1 +1,28 @@
-
+#!/bin/sh
+#########################################################################
+#
+# ABOUT THIS PROGRAM
+#
+# NAME
+#   ZeroTier Install Script
+#
+# SYNOPSIS
+# zerotier.sh
+#
+#########################################################################
+#
+# HISTORY
+#
+#   Version: 1.1
+#
+#   - 1.0 Martyn Watts, 25.06.2021 Initial Script Template Build
+#   - 1.1 Michael Tanner, 28.09.2021 Implement ZeroTier Install Script
+#
+#########################################################################
+# Script to download and install 1Password 7.
+#
+curl -o /tmp/apps/ZT.pkg https://download.zerotier.com/dist/ZeroTier%20One.pkg
+installer -pkg /tmp/apps/ZT.pkg -target /
+rm -rf /tmp/apps/ZT.pkg
+sleep 2s
+/usr/local/bin/zerotier-cli join $@
