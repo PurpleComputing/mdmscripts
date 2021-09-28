1770ba795c7123aec977a590e178d876ef328f70
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-09-28 19:12:16 +0100

    Update LibreOffice.sh

--- LibreOffice.sh
+++ LibreOffice.sh
@@ -1,112 +1,127 @@
-#!/bin/sh
-#####################################################################################################
+#!/bin/bash
+#########################################################################
 #
 # ABOUT THIS PROGRAM
 #
 # NAME
-#   FirefoxInstall.sh -- Installs or updates Firefox
+#   LibreOffice 
 #
 # SYNOPSIS
-#   sudo FirefoxInstall.sh
+#   sudo libreoffice.sh
 #
-####################################################################################################
+#########################################################################
 #
 # HISTORY
 #
-#   Version: 1.6
+#   Version: 1.0
 #
-#   - Joe Farage, 18.03.2015
-#   - Chris Hansen, 14.05.2020 Some square brackets change to double square brackets
-#   - Martyn Watts, 24.05.2020 Removed Language Variables as they are not needed and results were inconsistent
-#   - Martyn Watts, 30.06.2021 Added dock icon for all users using dockutil (prerequisite)
-#   - Michael Tanner, 18.08.2021 Adapted for use with Mosyle
-#   - Martyn Watts, 28.09.2021 Added Open Console Parameter to use with TeamViewer - Corrected Version numbering
+#   - 1.0 Martyn Watts, 28.09.2021 Initial Build
 #
-####################################################################################################
-# Script to download and install Firefox.
-# Only works on Intel systems.
+#########################################################################
+# Script to download and install LibreOffice.
 #
 
-dmgfile="FF.dmg"
-logfile="/Library/Logs/FirefoxInstallScript.log"
+releaseNotesUrl='https://www.libreoffice.org/download/download/'
+dnldfile='LibreOffice'
+appName='LibreOffice'
+forceQuit='Y'
+logfile="/Library/Logs/${appName}.log"
 deplog="/var/tmp/depnotify.log"
+userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X ${OSvers_URL}) AppleWebKit/535.6.2 (KHTML, like Gecko) Version/5.2 Safari/535.6.2"
 
-if [[ $@ == "openconsole" ]]; then
-	open ${logfile}
-	open ${deplog}
+
+/bin/echo "Status: Installing ${appName}" >> ${deplog}
+
+#  To get just the latest version number from the Release Notes URL
+/bin/echo "`date`: Getting latest version number" >> ${logfile}
+/bin/echo "Getting latest version number"
+latestver=$(curl -s -A ${userAgent} ${releaseNotesUrl} | grep -m 1 'dl_version_number' | cut -f3 -d'>' | cut -f1 -d'<')
+
+# To get the latest download link from the Release Notes URL
+dlurl=$(curl -s -A ${userAgent} ${releaseNotesUrl} | grep -m 1 'dl_download_link' | cut -f4 -d'"')
+dlrdurl=$(curl -A ${userAgent} ${dlurl} | iconv -f windows-1251  | grep -m1 'Refresh' | cut -f4 -d'=' | cut -f1 -d'"')
+url=$(curl -s -A ${userAgent} ${dlrdurl} | iconv -f windows-1251  | grep -m 1 'href' | cut -f2 -d'"')
+/bin/echo "`date`: Latest version number is: ${latestver}" >> ${logfile}
+/bin/echo "Latest version number is: ${latestver}"
+
+
+# Get the version number of the currently-installed App, if any.
+if [[ -e "/Applications/${appName}.app" ]]; then
+	currentinstalledver=`/usr/bin/defaults read "/Applications/${appName}.app/Contents/Info" CFBundleShortVersionString`
+	currentinstalledver=${currentinstalledver%.*}
+	/bin/echo "Current installed version is: $currentinstalledver"
+      /bin/echo "`date`: Current installed version is: $currentinstalledver" >> ${logfile}
+      if [[ ${latestver} = ${currentinstalledver} ]]; then
+      	/bin/echo "${appName} is current. Exiting"
+            /bin/echo "`date`: ${appName} is current. Exiting" >> ${logfile}
+            /bin/echo "Status: ${appName} is current. Exiting" >> ${deplog}
+            exit 0
+      fi
+else
+	currentinstalledver="none"
+	/bin/echo "${appName} is not installed”
+	/bin/echo "`date`: ${appName} is not installed" >> ${logfile}
 fi
 
-echo "Status: Installing Firefox" >> ${deplog}
 
-# Are we running on Intel?
-if [ '`/usr/bin/uname -p`'="i386" -o '`/usr/bin/uname -p`'="x86_64" ]; then
-	## Get OS version and adjust for use with the URL string
-	OSvers_URL=$( sw_vers -productVersion | sed 's/[.]/_/g' )
-
-	## Set the User Agent string for use with curl
-	userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X ${OSvers_URL}) AppleWebKit/535.6.2 (KHTML, like Gecko) Version/5.2 Safari/535.6.2"
-
-	# Get the latest version of Browser available from Firefox page.
-	latestver=`/usr/bin/curl -s -A "$userAgent" https://www.mozilla.org/en-US/firefox/new/ | grep 'data-latest-firefox' | sed -e 's/.* data-latest-firefox="\(.*\)".*/\1/' -e 's/\"//' | /usr/bin/awk '{print $1}'`
-	echo "Latest Version is: $latestver"
-
-	# Get the version number of the currently-installed FF, if any.
-	if [[ -e "/Applications/Firefox.app" ]]; then
-		currentinstalledver=`/usr/bin/defaults read /Applications/Firefox.app/Contents/Info CFBundleShortVersionString`
-		echo "Current installed version is: $currentinstalledver"
-		if [[ ${latestver} = ${currentinstalledver} ]]; then
-			echo "Firefox is current. Exiting"
-			exit 0
-		fi
-	else
-		currentinstalledver="none"
-		echo "Firefox is not installed"
-	fi
-
-	url="https://download-installer.cdn.mozilla.net/pub/firefox/releases/${latestver}/mac/en-US/Firefox%20${latestver}.dmg"
-	echo "Latest version of the URL is: $url"
-	echo "`date`: Download URL: $url" >> ${logfile}
-
-	# Compare the two versions, if they are different or Firefox is not present then download and install the new version.
-	if [[ "${currentinstalledver}" != "${latestver}" ]]; then
-		/bin/echo "`date`: Current Firefox version: ${currentinstalledver}" >> ${logfile}
-		/bin/echo "`date`: Available Firefox version: ${latestver}" >> ${logfile}
-		/bin/echo "`date`: Downloading newer version." >> ${logfile}
-		/usr/bin/curl -s -o /tmp/${dmgfile} ${url}
+# Compare the two versions, if they are different or the App is not present then download and install the new version.
+if [[ "${currentinstalledver}" != "${latestver}" ]]; then
+	/bin/echo "`date`: Current ${appName} version: ${currentinstalledver}" >> ${logfile}
+      /bin/echo "Current ${appName} version: ${currentinstalledver}"
+      /bin/echo "`date`: Available ${appName} version: ${latestver}" >> ${logfile}
+      /bin/echo "Available ${appName} version: ${latestver}"
+      /bin/echo "`date`: Downloading newer version." >> ${logfile}
+      /bin/echo "Downloading newer version."
+      /usr/bin/curl -s -o /tmp/${dnldfile} ${url}
+      if [[ "${forceQuit}" = "Y" ]]; then
+      	killall ${appName}
+      fi
+		##########################################################################
+		# Uncomment this block for dmg & .app copy          					 #
+		##########################################################################
 		/bin/echo "`date`: Mounting installer disk image." >> ${logfile}
-		/usr/bin/hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
-		/bin/echo "`date`: Installing..." >> ${logfile}
-		ditto -rsrc "/Volumes/Firefox/Firefox.app" "/Applications/Firefox.app"
-
+		/usr/bin/hdiutil attach /tmp/${appName} -nobrowse -quiet
+		/bin/echo "`date`: Installing..." >> ${logfile}	
+		ditto -rsrc "/Volumes/${appName}/${appName}.app" "/Applications/${appName}.app"
 		/bin/sleep 10
 		/bin/echo "`date`: Unmounting installer disk image." >> ${logfile}
-		/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep Firefox | awk '{print $1}') -quiet
+		/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep ${appName} | awk '{print $1}') -quiet
 		/bin/sleep 10
-		/bin/echo "`date`: Deleting disk image." >> ${logfile}
-		/bin/rm /tmp/${dmgfile}
+		##########################################################################
 
-		#double check to see if the new version got updated
-		newlyinstalledver=`/usr/bin/defaults read /Applications/Firefox.app/Contents/Info CFBundleShortVersionString`
-		if [[ "${latestver}" = "${newlyinstalledver}" ]]; then
-			/bin/echo "`date`: SUCCESS: Firefox has been updated to version ${newlyinstalledver}" >> ${logfile}
-			/bin/echo "`date`: Creating Dock Icon." >> ${logfile}
-			#/usr/local/bin/dockutil --remove 'Firefox' --allhomes
-			/bin/sleep 3
-			#/usr/local/bin/dockutil --add '/Applications/Firefox.app' --after 'Safari' --allhomes
-	   # /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -title "Firefox Installed" -description "Firefox has been updated." &
-		else
-			/bin/echo "`date`: ERROR: Firefox update unsuccessful, version remains at ${currentinstalledver}." >> ${logfile}
-			/bin/echo "--" >> ${logfile}
-			exit 1
-		fi
+       
+    	/bin/sleep 5
+      /bin/echo "`date`: Deleting the downloaded file." >> ${logfile}
+      /bin/rm /tmp/${dnldfile}
 
-	# If Firefox is up to date already, just log it and exit.       
-	else
-		/bin/echo "`date`: Firefox is already up to date, running ${currentinstalledver}." >> ${logfile}
-		/bin/echo "--" >> ${logfile}
-	fi  
+# Double check to see if the new version got updated
+newlyinstalledver=`/usr/bin/defaults read "/Applications/${appName}.app/Contents/Info" CFBundleShortVersionString`
+newlyinstalledver=${newlyinstalledver%.*}
+if [[ "${latestver}" = "${newlyinstalledver}" ]]; then
+	/bin/echo "`date`: SUCCESS: ${appName} has been updated to version ${newlyinstalledver}" >> ${logfile}
+      /bin/echo "SUCCESS: ${appName} has been updated to version ${newlyinstalledver}"
+      if [[ -e "/usr/local/bin/dockutil" ]]; then
+          	/bin/echo "`date`: Removing Existing Dock Icon." >> ${logfile}
+   		/bin/echo "Removing Existing Dock Icon."           
+	 	/usr/local/bin/dockutil --remove "${appName}" --allhomes >> ${logfile}
+		/bin/sleep 5
+		/bin/echo "`date`: Creating New Dock Icon." >> ${logfile}
+          	/bin/echo "Creating New Dock Icon."
+		/usr/local/bin/dockutil --add "/Applications/${appName}.app" --position 1  --allhomes >> ${logfile}
+            /bin/sleep 3
+       fi
+       /bin/echo "--" >> ${logfile}
 else
-	/bin/echo "`date`: ERROR: This script is for Intel Macs only." >> ${logfile}
+       /bin/echo "`date`: ERROR: ${appName} update unsuccessful, version remains at ${currentinstalledver}." >> ${logfile
+       /bin/echo "ERROR: ${appName} update unsuccessful, version remains at ${currentinstalledver}."
+       /bin/echo "--" >> ${logfile}
+       exit 1
 fi
-echo "Command: DeterminateManualStep: 1" >> ${deplog}
-exit 0
+
+else
+# If App is up to date already, just log it and exit.       
+        /bin/echo "`date`: ${appName} is already up to date, running ${currentinstalledver}." >> ${logfile}
+        /bin/echo "${appName} is already up to date, running ${currentinstalledver}."
+        /bin/echo "--" >> ${logfile}
+fi
+/bin/echo "Command: DeterminateManualStep: 1" >> ${deplog}
