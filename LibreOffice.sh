93a6c16e5799a23ec4f36382c14aa69de54190e0
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-09-29 08:46:53 +0100

    Update LibreOffice.sh

--- LibreOffice.sh
+++ LibreOffice.sh
@@ -77,7 +77,7 @@ if [[ "${currentinstalledver}" != "${latestver}" ]]; then
       /bin/echo "Available ${appName} version: ${latestver}"
       /bin/echo "`date`: Downloading newer version." >> ${logfile}
       /bin/echo "Downloading newer version."
-      /usr/bin/curl -A ${userAgent} -o /tmp/${dnldfile} ${url}
+      curl -o /tmp/${dnldfile} ${url}
       if [[ "${forceQuit}" = "Y" ]]; then
       	killall ${appName}
       fi
