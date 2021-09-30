8cb418a9ab29f3c2e429691cf67e67f9fb2c2a20
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-09-30 19:58:58 +0100

    Update !Script-Template.sh

--- !Script-Template.sh
+++ !Script-Template.sh
@@ -161,7 +161,7 @@
        	
        	# Unmount the DMG
        		/bin/echo "`date`: Unmounting installer disk image." >> ${logfile}
-       		/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep AppName | awk '{print $1}') -quiet
+       		/usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep "${appName}" | awk '{print $1}') -quiet
        		/bin/sleep 10
        
     else
