83ed93ef69f0f5a82c168d28efd8c1317e664eef
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-04 20:19:26 +0100

    Update brandDEPinstall.sh

--- Helpers/brandDEPinstall.sh
+++ Helpers/brandDEPinstall.sh
@@ -20,18 +20,18 @@
 #########################################################################
 # Script to brand DEPNotify with PurpleComputing for app installs.
 #
+APPINSTALLNAME=$(cat /tmp/.appinstallname)
 
 ## BRANDING DEPNOTIFY WINDOW
-echo 'Command: MainTitle: App Installation' >> /var/tmp/depnotify.log
+echo 'Command: MainTitle: Installing $APPINSTALLNAME...' >> /var/tmp/depnotify.log
 echo 'Command: Image: /Library/Application Support/Purple/logo.png' >> /var/tmp/depnotify.log
-echo 'Command: MainText: We are now installing the requested application.' >> /var/tmp/depnotify.log
+echo 'Command: MainText: We are now installing re-installing the $APPINSTALLNAME application.' >> /var/tmp/depnotify.log
 
 ## STARTING INSTALLS ###
 echo 'Status: Starting MDM Installer' >> /var/tmp/depnotify.log
 sleep 3s
 #sudo -u $(stat -f '%Su' /dev/console) /bin/sh <<'END'
 chmod 777 /var/tmp/depnotify.log
-killall DEPNotify
 /Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify
 chmod 777 /var/tmp/depnotify.log
 sleep 3s
