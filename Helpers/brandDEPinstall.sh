406069031171831bc99a854eeae20195c2e872cf
Michael <30936296+mtan93@users.noreply.github.com>
2021-10-04 20:09:58 +0100

    Update brandDEPinstall.sh

--- Helpers/brandDEPinstall.sh
+++ Helpers/brandDEPinstall.sh
@@ -27,9 +27,12 @@ echo 'Command: Image: /Library/Application Support/Purple/logo.png' >> /var/tmp/
 echo 'Command: MainText: Your organisation is using a Mobile Device Management solution provided by Purple Computing, we are now installing your organisations software, settings and policies.' >> /var/tmp/depnotify.log
 
 ## STARTING INSTALLS ###
-echo 'Status: Starting Software Installs' >> /var/tmp/depnotify.log
+echo 'Status: Starting MDM Installer' >> /var/tmp/depnotify.log
+sleep 3s
 #sudo -u $(stat -f '%Su' /dev/console) /bin/sh <<'END'
 chmod 777 /var/tmp/depnotify.log
 killall DEPNotify
 /Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify
 chmod 777 /var/tmp/depnotify.log
+sleep 3s
+echo 'Status: Starting Software Install' >> /var/tmp/depnotify.log
