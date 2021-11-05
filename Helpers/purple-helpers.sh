261786b9f71d46b81f450e9839cbceaec7f48541
Michael <30936296+mtan93@users.noreply.github.com>
2021-11-05 21:49:24 +0000

    add purple tmp folder

--- Helpers/purple-helpers.sh
+++ Helpers/purple-helpers.sh
@@ -7,5 +7,7 @@ curl -o /Library/Application\ Support/Purple/logo-dark.png https://raw.githubuse
 curl -o /Library/Application\ Support/Purple/purple-icon.png https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/PurpleLogos/purple-icon.png
 curl -o /Library/Application\ Support/Purple/launch-dep.sh https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/launch-dep.sh
 curl -o /Library/Application\ Support/Purple/launch-dep-en.sh https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/launch-dep-en.sh
+mkdir -p /private/var/tmp/purple/
 # Give full permissions
 chmod -R 777 /Library/Application\ Support/Purple/
+chmod -R 777 /private/var/tmp/purple/
