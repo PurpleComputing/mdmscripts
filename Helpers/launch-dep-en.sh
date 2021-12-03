486c7bdf0f9ba70460d73e04bac461299d380b23
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-12-03 16:25:50 +0000

    + sed to rewrite any legacy 'Quit' or Restart' cmd

--- Helpers/launch-dep-en.sh
+++ Helpers/launch-dep-en.sh
@@ -1,3 +1,5 @@
 # LAUNCH DEPNOTIFY FOR ENROLMENT
+sed -i -e 's/Quit/Q*uit/' /var/tmp/depnotify.log
+sed -i -e 's/Restart/R*estart/' /var/tmp/depnotify.log
 sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
 open -a /Applications/Utilities/DEPNotify.app --args -fullScreen
--- Helpers/launch-dep.sh
+++ Helpers/launch-dep.sh
@@ -1,1 +1,3 @@
+sed -i -e 's/Quit/Q*uit/' /var/tmp/depnotify.log
+sed -i -e 's/Restart/R*estart/' /var/tmp/depnotify.log
 bash -c 'osascript -e "tell application \"DEPNotify\"" -e "activate" -e "end tell"'
