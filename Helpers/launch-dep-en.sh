6f47fcad1828210e0c8e1bded2d2012dc60e05fe
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-12-03 13:42:27 +0000

    update

--- Helpers/launch-dep-en.sh
+++ Helpers/launch-dep-en.sh
@@ -1,3 +1,3 @@
 # LAUNCH DEPNOTIFY FOR ENROLMENT
-screen -dmS DEPEnroll /Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify -fullScreen
-# /Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify -fullScreen
+#screen -dmS DEPEnroll /Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify -fullScreen
+/Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify -fullScreen &
