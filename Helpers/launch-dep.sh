ca5a453098e5068da57147b51a60f3181980a938
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-12-03 15:47:07 +0000

    update

--- Helpers/launch-dep.sh
+++ Helpers/launch-dep.sh
@@ -1,3 +1,4 @@
 #bash -c 'osascript -e "tell application \"DEPNotify\"" -e "activate" -e "end tell"'
+sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
 currentUser=$(id -un)
 sudo -u $currentUser open -a /Applications/Utilities/DEPNotify.app --args -fullScreen
