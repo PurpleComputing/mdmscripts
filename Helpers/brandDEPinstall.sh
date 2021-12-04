c4f39e11965d8260a2b1621370f49e4087f628c8
Michael <30936296+mtan93@users.noreply.github.com>
2021-12-04 16:24:51 +0000

    Fixing app name location

--- Helpers/brandDEPinstall.sh
+++ Helpers/brandDEPinstall.sh
@@ -20,13 +20,15 @@
 #########################################################################
 # Script to brand DEPNotify with PurpleComputing for app installs.
 #
-appname=$(cat /tmp/.appinstallname)
+cp -f /tmp/.appinstallname /Library/Caches/com.purplecomputing.mdm/Apps/.appinstallname
+rm /tmp/.appinstallname
+appname=$(cat /Library/Caches/com.purplecomputing.mdm/Apps/.appinstallname)
 chmod 777 /var/tmp/depnotify.log
 
 ## BRANDING DEPNOTIFY WINDOW
 echo Command: MainTitle: Installing $appname... >> /var/tmp/depnotify.log
 echo 'Command: Image: /Library/Application Support/Purple/logo.png' >> /var/tmp/depnotify.log
-echo Command: MainText: We are now installing re-installing the $appname application. >> /var/tmp/depnotify.log
+echo 'Command: MainText: We are now installing/re-installing the $appname application.' >> /var/tmp/depnotify.log
 echo Command: WindowStyle: Activate >> /var/tmp/depnotify.log
 echo Command: NotificationOn: >> /var/tmp/depnotify.log
 ## STARTING INSTALLS ###
