03aba2c1e090a9f9c3450dcc8203d62647302ef2
wattsy74 <49881777+wattsy74@users.noreply.github.com>
2021-12-16 17:13:30 +0000

    update

--- computer-rename2.sh
+++ computer-rename2.sh
@@ -2,7 +2,7 @@
 
 FullName=$(sudo -u $(stat -f "%Su" /dev/console) id -F)
 ComputerModel=$(/usr/sbin/system_profiler SPHardwareDataType | grep 'Model Name' | cut -d":" -f2)
-ComputerName=$(echo $FullName$ComputerModel | sed -e "s/ /-/g" )
+ComputerName=$(echo $FullName$ComputerModel | sed -e "s/ /-/g" | sed -e "s/\./-/g" )
 /usr/sbin/scutil --set HostName "$ComputerName"
 /usr/sbin/scutil --set LocalHostName "$ComputerName"
 /usr/sbin/scutil --set ComputerName "$ComputerName"
