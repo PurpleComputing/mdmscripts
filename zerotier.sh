fbf8dee14de0d08e0cace7d904b85ef1bfed275e
Michael <30936296+mtan93@users.noreply.github.com>
2022-08-04 10:42:14 +0100

    Update zerotier.sh

--- zerotier.sh
+++ zerotier.sh
@@ -39,9 +39,7 @@ echo "Status: Installing ${appName} using script version ${scriptver}" >> ${logf
 echo "Status: Downloading ZeroTier" >> ${logfile}
 echo "Status: Downloading ZeroTier" >> ${deplog}
 
-# Using 1.8.3 version to stop the continual prompts for credentials
-curl -o /Library/Caches/com.purplecomputing.mdm/Apps/ZT.pkg https://download.zerotier.com/RELEASES/1.8.3/dist/ZeroTierOne.pkg
-#curl -o /Library/Caches/com.purplecomputing.mdm/Apps/ZT.pkg https://download.zerotier.com/dist/ZeroTier%20One.pkg
+curl -o /Library/Caches/com.purplecomputing.mdm/Apps/ZT.pkg https://download.zerotier.com/dist/ZeroTier%20One.pkg
 
 echo "Status: Running the installer" >> ${logfile}
 echo "Status: Running the installer" >> ${deplog}
