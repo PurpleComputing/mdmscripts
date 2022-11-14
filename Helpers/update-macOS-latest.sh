ee6ad52ddaa241334026cbd9f2d18db54768acd8
Michael <30936296+mtan93@users.noreply.github.com>
2022-11-14 15:08:22 +0000

    Create update-macOS-latest.sh

---
+++ Helpers/update-macOS-latest.sh
@@ -0,0 +1,18 @@
+#DEVSCRIPT
+
+#CHECKS FOR REQUIRED FILES
+if [ ! -e "/usr/local/Installomator/Installomator.sh" ]; then
+	curl -s https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Installomator.sh?v=123$(date +%s) | bash
+fi
+if [ ! -e "/usr/local/bin/dialog" ]; then
+	/usr/local/Installomator/Installomator.sh swiftdialog NOTIFY=silent BLOCKING_PROCESS_ACTION=kill
+fi
+if [ ! -e "/Applications/Utilities/DEPNotify.app/Contents/MacOS/DEPNotify" ]; then
+	/usr/local/Installomator/Installomator.sh depnotify NOTIFY=silent BLOCKING_PROCESS_ACTION=kill
+fi
+
+#PROMPTS WITH INSTALL
+/usr/local/bin/dialog dialog --title "Purple Managed macOS Updates" --message "**WARNING: Restart Required** \n\nYou are about to install an OS update which could take up to 1 hour 30 minutes to install. \n\n Please confirm you have saved all work and wish to continue?" --blurscreen --icon "https://www.apple.com/newsroom/images/product/os/macos/standard/Apple-WWDC22-macOS-Ventura-Spotlight-show-220606_big.jpg.large.jpg" --overlayicon warning --ontop --button1text "Continue" --button1shellaction "curl -s https://raw.githubusercontent.com/grahampugh/erase-install/main/erase-install.sh | sudo bash /dev/stdin --reinstall --depnotify --check-power --power-wait-limit 180" --button2text "Later"
+
+#EXITS
+exit 0
