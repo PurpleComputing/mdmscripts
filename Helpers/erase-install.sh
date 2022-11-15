d3d3283e3af4a71eee25e8e8cb9fe55afe8ecf3e
Michael <30936296+mtan93@users.noreply.github.com>
2022-11-15 12:14:41 +0000

    Update erase-install.sh

--- Helpers/erase-install.sh
+++ Helpers/erase-install.sh
@@ -17,7 +17,7 @@ fi
 
 if [ "$SHOWDIALOG" == "Y" ]; then
 	echo "Dialog will open"
-	/usr/local/bin/dialog dialog --title "Purple Managed macOS Erase" --message "**WARNING: MAC WILL SELF DESTRUCT!** \n\nYou are about to erase and re-install your Mac which could take up to 1 hour 30 minutes to complete. \n\n Please confirm you have saved all work to the cloud and have a full backup (if required)?" --blurscreen --icon "https://www.apple.com/newsroom/images/product/os/macos/standard/Apple-WWDC22-macOS-Ventura-Spotlight-show-220606_big.jpg.large.jpg" --overlayicon xmark.circle --ontop --button1text "Continue" --button1shellaction "$EIRUNCOMMAND" --button2text "Later"
+	/usr/local/bin/dialog dialog --title "Purple Managed macOS Erase" --message "**WARNING: MAC WILL SELF DESTRUCT!** \n\nYou are about to erase and re-install your Mac which could take up to 1 hour 30 minutes to complete. \n\n Please confirm you have saved all work to the cloud and have a full backup (if required)?" --blurscreen --icon "https://www.apple.com/newsroom/images/product/os/macos/standard/Apple-WWDC22-macOS-Ventura-Spotlight-show-220606_big.jpg.large.jpg" --overlayicon warning --ontop --button1text "Continue" --button1shellaction "$EIRUNCOMMAND" --button2text "Later"
 
 else
 	echo "Dialog will not open"
