5ee180be6706f0de6bf2509cf08fa9c975360749
Michael <30936296+mtan93@users.noreply.github.com>
2022-11-27 15:07:09 +0000

    quoted out URLS

--- Helpers/launch-app-installs.sh
+++ Helpers/launch-app-installs.sh
@@ -3,8 +3,8 @@
 # Installation using Installomator with Dialog showing progress (and possibility of adding to the Dock)
 echo "*** USING BETA SCRIPT: LAUNCH ***"
 
-curl -s https://raw.githubusercontent.com/Installomator/Installomator/main/MDM/install%20swiftDialog%20direct.sh | bash
-curl -s https://raw.githubusercontent.com/Installomator/Installomator/main/MDM/install%20Installomator%20direct.sh | bash
+curl -s "https://raw.githubusercontent.com/Installomator/Installomator/main/MDM/install%20swiftDialog%20direct.sh" | bash
+curl -s "https://raw.githubusercontent.com/Installomator/Installomator/main/MDM/install%20Installomator%20direct.sh" | bash
 
 whatList="$MDMAPPLABEL"
 
