ad77d7819c31575fec7ef32c9e2f4a292b16216e
Michael <30936296+mtan93@users.noreply.github.com>
2022-11-27 15:01:31 +0000

    testing

--- Helpers/launch-app-installo.sh
+++ Helpers/launch-app-installo.sh
@@ -4,16 +4,22 @@
 # SERVICE SCRIPT CALLED BY OTHER SCRIPTS
 
 # INSTALLS APPLICATION
-sleep 5
-curl -s https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/install-app-loop-beta.sh?v=123$(date +%s) | bash
 
-# CHECKS TO ADD TO DOCK OR NOT
-if [ "$MDMADDTODOCK" == "Y" ]; then
-	echo "SCRIPT WILL TRY TO ADD TO DOCK"
-	curl -s https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/add-to-dock.sh?v=123$(date +%s) | bash
-	sleep 2
-	killall Dock
-else
-	echo "SCRIPT WILL NOT TRY TO ADD TO DOCK"
-	echo Continuing...
-fi
+## BELOW CODE COMMENTED OUT
+# sleep 5
+# curl -s https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/install-app-loop-beta.sh?v=123$(date +%s) | bash
+# 
+# # CHECKS TO ADD TO DOCK OR NOT
+# if [ "$MDMADDTODOCK" == "Y" ]; then
+# 	echo "SCRIPT WILL TRY TO ADD TO DOCK"
+# 	curl -s https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/add-to-dock.sh?v=123$(date +%s) | bash
+# 	sleep 2
+# 	killall Dock
+# else
+# 	echo "SCRIPT WILL NOT TRY TO ADD TO DOCK"
+# 	echo Continuing...
+# fi
+# ABOVE CODE COMMENTED OUT
+
+# NEW SCRIPT
+curl -s -L "https://raw.githubusercontent.com/PurpleComputing/mdmscripts/main/Helpers/launch-app-installs.sh?v=123"$(date +%s) | bash
