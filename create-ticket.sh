edfce193a0b6fefbc1425f2aab70ac069c70e4d2
Michael <30936296+mtan93@users.noreply.github.com>
2022-12-03 16:09:34 +0000

    Add files via upload

---
+++ create-ticket.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+VARTITLE="Create Ticket"
+VARURL="https://www.cognitoforms.com/PurpleComputingLimited/contactandsupportform"
+VARLOGO="https://raw.githubusercontent.com/PurpleComputing/image-repo/main/logo-padded.png"
+
+/usr/local/bin/dialog dialog --webcontent $VARURL --big --title "$VARTITLE"  --message "" --icon $VARLOGO --overlayicon SF=envelope.badge.shield.half.filled --button1text "Close" --infobuttontext "Open in Browser" --infobuttonaction "https://purplecomputing.com/support" -d 
