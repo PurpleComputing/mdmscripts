ab2fd966efc7725d9acbad82b0ad225823b267c9
Michael <30936296+mtan93@users.noreply.github.com>
2023-06-26 15:20:14 +0100

    Update join-zt-network.sh

--- Helpers/join-zt-network.sh
+++ Helpers/join-zt-network.sh
@@ -66,7 +66,7 @@ sleep 3
 #
 
 curl -H "Authorization: Bearer $ZTAPIKEY" -X POST -d '{"name":"'"$ZTDEVICENAME"'","description":"Device authorised through Purple Script.","config":{"authorized":true}}' https://api.zerotier.com/api/v1/network/$ZTNETID/member/$MYID
-curl -s -H "Authorization: Bearer $ZTAPIKEY" https://my.zerotier.com/api/network/$ZTNETID/member/$MYID
+curl -s -H "Authorization: Bearer $ZTAPIKEY"https://api.zerotier.com/api/v1/network/$ZTNETID/member/$MYID
 echo Status: Network authorised, ready to go! >> /var/tmp/depnotify.log
 touch /tmp/ztnetauthed.log
 
