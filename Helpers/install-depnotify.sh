dbbb3c8ed65793a2138be6e929e6041d332dc162
Michael <30936296+mtan93@users.noreply.github.com>
2023-11-27 20:23:51 +0000

    Create install-depnotify.sh

---
+++ Helpers/install-depnotify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# URL of the .pkg file to download
+downloadUrl="https://store.prpl.it/DEPNotify.pkg"
+
+# Destination directory to store the downloaded .pkg file
+destinationDirectory="/tmp/pkg-install"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$destinationDirectory"
+
+# Download the .pkg file
+curl -o "$destinationDirectory/DEPNotify.pkg" "$downloadUrl"
+
+# Install the .pkg file
+installer -pkg "$destinationDirectory/DEPNotify.pkg" -target /
