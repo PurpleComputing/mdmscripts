b1b04d5a6a30b238e5366a8aead943dc0d286a93
Michael <30936296+mtan93@users.noreply.github.com>
2023-12-29 13:39:23 +0000

    Update Rosetta2.sh

--- Rosetta2.sh
+++ Rosetta2.sh
@@ -23,17 +23,18 @@
 ####################################################################################################
 # Script to identify architecture and install Rosetta2 if needed
 #
-
+appName="Rosetta 2"
 logfile="/Library/Caches/com.purplecomputing.mdm/Logs/Rosetta2InstallScript.log"
 deplog="/var/tmp/depnotify.log"
 scriptver="1.2"
 
 # Making Purple Cache directories for in the event that the helper script hasn't been run
-mkdir -p /Library/Caches/com.purplecomputing.mdm/
-mkdir -p /Library/Caches/com.purplecomputing.mdm/Logs/
+
+
+mkdir -p /Library/Logs/com.purplecomputing.mdm/
 mkdir -p /Library/Caches/com.purplecomputing.mdm/Apps/
 
-/bin/echo "Status: Installing ${appName}" >> ${deplog}
+/bin/echo "Status: Installing ${appName} ..." >> ${deplog}
 /bin/echo "Status: Installing ${appName}" >> ${logfile}
 
 if [[ $1 == "openconsole" ||  $2 == "openconsole" ]]; then
@@ -45,30 +46,29 @@ fi
 
 arch=$(/usr/bin/arch)
 if [ "$arch" == "arm64" ]; then
-    echo "Apple Silicon - Installing Rosetta" >> ${logfile}
-    echo "Apple Silicon - Installing Rosetta" >> ${deplog}
-    echo "Apple Silicon - Installing Rosetta"
+    echo "Status: Apple Silicon - Installing Rosetta ..." >> ${logfile}
+    echo "Status: Apple Silicon - Installing Rosetta" >> ${deplog}
+    echo "Status: Apple Silicon - Installing Rosetta"
     sleep 1
     /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-    echo "Rosetta 2 Now Installed" >> ${deplog}
-    echo "Rosetta 2 Now Installed" >> ${logfile}
-    echo "Rosetta 2 Now Installed" 
+    echo "Status: Rosetta 2 Now Installed ..." >> ${deplog}
+    echo "Status: Rosetta 2 Now Installed" >> ${logfile}
+    echo "Status: Rosetta 2 Now Installed" 
     sleep 2
 elif [ "$arch" == "i386" ]; then
-    echo "Intel - Skipping Rosetta" >> ${deplog}
-    echo "Intel - Skipping Rosetta" >> ${logfile}
-    echo "Intel - Skipping Rosetta"
-    sleep 1
+    echo "Status: Intel - Skipping Rosetta ..." >> ${deplog}
+    echo "Status: Intel - Skipping Rosetta" >> ${logfile}
+    echo "Status: Intel - Skipping Rosetta"
+    sleep 3
 else
-    echo "Unknown Architecture" >> ${deplog}
-    echo "Unknown Architecture" >> ${logfile}
-    echo "Unknown Architecture"
-    sleep 1
+    echo "Status: Unknown Architecture..." >> ${deplog}
+    echo "Status: Unknown Architecture" >> ${logfile}
+    echo "Status: Unknown Architecture"
+    sleep 3
 fi
 
 if [[ $1 == "jamf" ||  $2 == "jamf" ]]; then
 echo "Found the jamf parameter so running a recon" >> ${logfile}
-echo "Running recon so Architecture gets updated and arch based smart groups work" >> ${deplog}
 echo "Running recon so Architecture gets updated and arch based smart groups work" >> ${logfile}
 jamf recon
 fi
